6e75432593c5aa37024d014abf063bb2cfee4b01
duong130125 <147391418+duong130125@users.noreply.github.com>
2025-05-12 10:33:17 +0700

    Update ConnectionDB.java

--- src/main/java/com/rikkei/ss05/utils/ConnectionDB.java
+++ src/main/java/com/rikkei/ss05/utils/ConnectionDB.java
@@ -9,7 +9,7 @@ public class ConnectionDB {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/JSP_Servlet_DB";
     private static final String USER_NAME = "root";
-    private static final String PASSWORD = "Lhn09112005@";
+    private static final String PASSWORD = "13012005";
 
     public static Connection openConnection() {
         Connection conn = null;
